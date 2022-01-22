1a9f2a04662da3484f78646e19880a3d601dc1d3
austinKwang <austinkvong@gmail.com>
2022-01-23 00:17:28 +0800

    init debugModule

---
+++ spring-debug-test/src/main/java/com/spring/debug/component/MyCalculator.java
@@ -0,0 +1,30 @@
+package com.spring.debug.component;
+
+/**
+ * @Author austin
+ * @Description
+ * @Date 8/17/21 11:16 PM
+ */
+public class MyCalculator {
+
+    public int add(int i, int j) {
+        int result = i + j;
+        return result;
+    }
+
+    public int sub(int i, int j) {
+        int result = i - j;
+        return result;
+    }
+
+    public int mult(int i, int j) {
+        int result = i * j;
+        return result;
+    }
+
+    public int div(int i, int j) {
+        int result = i / j;
+        return result;
+    }
+
+}
---
+++ spring-debug-test/src/main/java/com/spring/debug/startspring.java
@@ -0,0 +1,26 @@
+package com.spring.debug;
+
+import com.spring.debug.component.MyCalculator;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * @Author startspring
+ * @Description
+ * @Date 10/11/21 12:42 AM
+ */
+public class startspring {
+	public static void main(String[] args) {
+
+		ApplicationContext applicationContext = getApplicationContext();
+		MyCalculator myCalculator = applicationContext.getBean(MyCalculator.class);
+		System.out.println(myCalculator.add(1, 6));
+	}
+
+	private static ApplicationContext getApplicationContext() {
+		ApplicationContext applicationContext = null;
+//        applicationContext = new AnnotationConfigApplicationContext(SpringConfig.class);
+		applicationContext = new ClassPathXmlApplicationContext("application.xml");
+		return applicationContext;
+	}
+}
